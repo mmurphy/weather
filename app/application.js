8d671ce7a5a2f2b7380a00b0ad1f6b37e8b67b71
Martin Murphy <mmurphy@redhat.com>
2021-04-27 18:09:51 +0100

    Add simple health check

--- app/application.js
+++ app/application.js
@@ -3,6 +3,11 @@ var app = require('express')();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.get('/healthz', function (req, res) {
+    console.log('health request from %s', req.ip);
+    res.send({});
+});
+
 app.get('/', function (req, res) {
     console.log('responding to request from %s', req.ip);
     res.send('Hello World!');
